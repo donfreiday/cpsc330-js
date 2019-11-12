9204bd03fcb1b57b3708ad76b3498a5bded190ad
Don Freiday <donfreiday@gmail.com>
2019-11-12 14:34:54 -0500

    Fix forms example

--- forms/forms.js
+++ forms/forms.js
@@ -1,12 +1,10 @@
 // CPSC330 JavaScript: Forms
 
 var ul = document.getElementById("list");
-
-var btnSubmit = document.getElementById("btnSubmit");
-btnSubmit.addEventListener("click", function () {
+function appendName(form) {
     // (1) Fill in code here to add the form contents to our ul
     // Hint: ul is provided above in global scope
     //       li's can be created with document.createElement("li")
     //       Add text to li with document.appendChild(str)
     //       Add li to ul with ul.appendChild(li)
-});
+};
