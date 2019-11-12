3978d04396de1f97407480a618413205d7353ad2
Don Freiday <donfreiday@gmail.com>
2019-11-12 11:21:47 -0500

    Complete forms example

--- forms/forms.js
+++ forms/forms.js
@@ -1,4 +1,12 @@
 // CPSC330 JavaScript: Forms
 
-// Consider using:
-// https://www.javaworld.com/article/2077176/using-javascript-and-forms.html
+var ul = document.getElementById("list");
+
+var btnSubmit = document.getElementById("btnSubmit");
+btnSubmit.addEventListener("click", function () {
+    // (1) Fill in code here to add the form contents to our ul
+    // Hint: ul is provided above in global scope
+    //       li's can be created with document.createElement("li")
+    //       Add text to li with document.appendChild(str)
+    //       Add li to ul with ul.appendChild(li)
+});
