8526286ca657d9771f07b58cc05f2ce9d10d834e
Don Freiday <donfreiday@gmail.com>
2019-11-12 14:36:12 -0500

    Cleanup forms example

--- forms/forms.js
+++ forms/forms.js
@@ -1,9 +1,8 @@
 // CPSC330 JavaScript: Forms
 
-var ul = document.getElementById("list");
 function appendName(form) {
     // (1) Fill in code here to add the form contents to our ul
-    // Hint: ul is provided above in global scope
+    // Hint: ul is document.getElementById("list");
     //       li's can be created with document.createElement("li")
     //       Add text to li with document.appendChild(str)
     //       Add li to ul with ul.appendChild(li)
